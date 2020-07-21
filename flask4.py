6287e0489eb2668b64e296d131b1c42ffefc9202
Nishant Kumar Prasad <45433024+iamnishantk@users.noreply.github.com>
2020-07-21 16:10:37 +0530

    Update flask4.py

--- flask4.py
+++ flask4.py
@@ -6,3 +6,11 @@ min=3;
 print(var);
 var1=random.randint(min,max);
 print(var1);
+
+
+from boltiot import Bolt
+api_key = "7aa8ddf9-d2c1-4f78-a309-6b5ab29c12fb"
+device_id  = "BOLT11692813"
+mybolt = Bolt(api_key, device_id)
+response = mybolt.isOnline()
+print (response)
